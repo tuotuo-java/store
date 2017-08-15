7df7e8233e613ae5535e53e1773daf31642ab110
wan <1213037845@qq.com>
2017-08-15 20:08:22 +0800

    snake3

--- src/main/java/com/tuotuo/snaker/Egg.java
+++ src/main/java/com/tuotuo/snaker/Egg.java
@@ -1,5 +1,6 @@
 package com.tuotuo.snaker;
 
+import java.awt.*;
 import java.util.Random;
 
 /**
@@ -12,5 +13,39 @@ public class Egg {
     private static Random r=new Random();
     private Color color=Color.GREEN;
 
-    Public Egg(int row,int col)
+    public Egg(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+    public Egg(){
+     this(r.nextInt(Yard.ROWS-2)+2,r.nextInt(Yard.COLS)));
+    }
+    public void reAppear(){
+        this.row=r.nextInt(Yard.ROWS-2)+2;
+        this.col=r.nextInt(Yard.COLS);
+    }
+    public void draw(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(color);
+        g.fillOval(Yard.BLOCK_SIZE * col, Yard.BLOCK_SIZE * row, w, h);
+        g.setColor(c);
+        if (color == Color.GREEN)
+            color = Color.RED;
+        else
+            color = Color.GREEN;
+    }
+    public int getCol(){
+    return  col;
+    }
+    public void setCol(int col){
+        this.col=col;
+    }
+    public int getRow(){
+        return row;
+    }
+    public void setRow(int row){
+        this.row=row;
+    }
 }
+
+
--- src/main/java/com/tuotuo/snaker/Snake.java
+++ src/main/java/com/tuotuo/snaker/Snake.java
@@ -1,7 +1,105 @@
 package com.tuotuo.snaker;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 /**
  * Created by 12130 on 2017/8/1.
  */
 public class Snake {
+    private Node head=null;
+    private Node tail=null;
+    private int size=0;
+
+    private Node n=new Node(20,30,Dir.L);
+    private Yard y;
+
+    public Snake(Yard y){
+        head=n;
+        tail=n;
+        size=1;
+        this.y=y;
+    }
+
+    public void addToTail(){
+        Node node=null;
+        switch(tail.dir){
+            case L:
+                node=new Node(tail.row,tail.col+1,tail.dir);
+                break;
+            case U:
+                node=new Node(tail.row+1,tail.col,tail.dir);
+                break;
+            case R:
+                node=new Node(tail.row,tail.col-1,tail.dir);
+                break;
+            case D:
+                nose=new Node(tail.row-1,tail.col,tail.dir);
+                break;
+        }
+        tail.next=node;
+        node.prev=tail;
+        tail=node;
+        size++;
+    }
+public void addToHead(){
+        Node node=null;
+        switch(head.dir) {
+            case L:
+                node = new Node(head.row, head.col - 1, head.dir);
+                break;
+            case U:
+                node = new Node(head.row - 1, head.col, head.dir);
+                break;
+            case R:
+                node = new Node(head.row, head.col + 1, head.dir);
+                break;
+            case D:
+                node = new Node(head.row + 1, head.col, head.dir);
+                break;
+            }
+        node.next=head;
+        head.prev=node;
+        head=node;
+        size++;
+        }
+public void draw(Graphics g){
+    Color c=g.getColor();
+    g.setColor(Color.BLACK);
+    g.fillRect(Yard.BLOCK_SIZE*col,Yard.BLOCK_SIZE*row,w,h,);
+    g.setColor(c);
+    }
+public void eat(Egg e) {
+        if (this.getRect().intersects(e.getREct())) {
+            e.reAppear();
+            this.addToHead();
+            y.setScore(y.getScore() + 5);
+        }
+    }
+public Rectangle getRect(){
+    return new Rectangle(Yard.BLOCK_SIZE*head.col,Yara.BLOCK_SIZE*head.row,head.w,head.h);
+    }
+
+public void keyPressed1(KeyEvent e){
+    int key=e.getKeyCode();
+    switch (key){
+        case KeyEvent.VK_LEFT:
+            if(head.dir!=Dir.R)
+                head.dir=Dir.l;
+            break;
+        case KeyEvent.VK_UP:
+            if(head.dir!=Dir.D)
+                head.dir=Dir.U;
+            break;
+        case KeyEvent.VK_RIGHT:
+            if(head.dir!=Dir.L)
+                head.dir=Dir.R;
+        case KeyEvent.VK_DOWN:
+            if(hea.dir!=Dir.U)
+                head.dir=Dir.D;
+            break;
+    }
 }
+
+}
+
